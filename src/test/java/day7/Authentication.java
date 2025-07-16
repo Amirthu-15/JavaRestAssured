18f39a6fdde3ccc8d2d6dd59788caafb928f855c
Amirtharaj <amirtharaj15790@gmail.com>
2025-07-16 20:58:17 +0530

    RestSecond

--- src/test/java/day7/Authentication.java
+++ src/test/java/day7/Authentication.java
@@ -48,7 +48,8 @@ public class Authentication {
 	@Test(priority = 4)
 	public void testBearerTokenAuthentication() {
 
-		String bearerToken = System.getenv("GITHUB_TOKEN");
+		String bearerToken = System.getenv("GITHUB_TOKEN"); // In Eclipse: Right-click your project → Run As → Run
+															// Configurations → Environment → New...
 
 		given()
 
@@ -56,15 +57,12 @@ public class Authentication {
 
 				.when().get("https://api.github.com/user/repos")
 
-				.then()
-				.statusCode(200)
-				.log().all();
+				.then().statusCode(200).log().all();
 
 	}
-	
-	@Test(priority = 5, enabled=false)
-	public void testOauthAuthentication() {
 
+	@Test(priority = 5, enabled = false)
+	public void testOauthAuthentication() {
 
 		given()
 				// This is oauth 1.0 Authentication
@@ -72,15 +70,12 @@ public class Authentication {
 
 				.when().get("url")
 
-				.then()
-				.statusCode(200)
-				.log().all();
+				.then().statusCode(200).log().all();
 
 	}
-	
-	@Test(priority = 6, enabled=false)
-	public void testOauth2Authentication() {
 
+	@Test(priority = 6, enabled = false)
+	public void testOauth2Authentication() {
 
 		given()
 				// This is oauth 2.0 Authentication
@@ -88,15 +83,12 @@ public class Authentication {
 
 				.when().get("url")
 
-				.then()
-				.statusCode(200)
-				.log().all();
+				.then().statusCode(200).log().all();
 
 	}
-	
-	@Test(priority = 7, enabled=false)
-	public void testApiAuthentication1() {
 
+	@Test(priority = 7, enabled = false)
+	public void testApiAuthentication1() {
 
 		given()
 				// This is Api Authentication
@@ -104,35 +96,29 @@ public class Authentication {
 
 				.when().get("https://api.openweathermap.org/data/2.5/forecast/daily?q=Delhi&units=metric&cnt=7")
 
-				.then()
-				.statusCode(200)
-				.log().all();
+				.then().statusCode(200).log().all();
 
 	}
-	
-	@Test(priority = 7, enabled=false)
-	public void testApiAuthentication2() {
 
+	@Test(priority = 7, enabled = false)
+	public void testApiAuthentication2() {
 
 		given()
 				// This is Api Authentication
-				
-				.pathParam("myPath", "data/2.5/forecast/daily")
-				.queryParam("appid", "f58b4bfa57e15013799775cfc76546c5")
-				
+
+				.pathParam("myPath", "data/2.5/forecast/daily").queryParam("appid", "f58b4bfa57e15013799775cfc76546c5")
+
 				.queryParam("q", "Delhi")
-				
+
 				.queryParam("units", "metric")
-				
+
 				.queryParam("cnt", "7")
 
 				.when()
-				
+
 				.get("https://api.openweathermap.org/{myPath}")
 
-				.then()
-				.statusCode(200)
-				.log().all();
+				.then().statusCode(200).log().all();
 
 	}
 
